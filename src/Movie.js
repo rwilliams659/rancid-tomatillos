aafb5bf47c96c0c0558e2bdac131bbb69b73ceb6
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-18 17:27:19 -0600

    Setup movies component

---
+++ src/Movie.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Movie = () => {
+  return <h1>Hello!!</h1>
+}
+
+export default Movie
---
+++ src/Movies.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Movie from './Movie'
+
+const Movies = ({movies}) => {
+  const movieList = movies.map(movie => <Movie key={movie.id} movie={movie} />)
+  return (
+    <section className='Movies'>
+      {movieList}
+    </section>
+  )
+}
+
+export default Movies
