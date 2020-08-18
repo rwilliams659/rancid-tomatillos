35b7dec3a1f208c1efdc931ce1e6187602034d33
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-18 17:13:37 -0600

    Setup header component

--- src/Header.js
+++ src/Header.js
@@ -1,3 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+class Header extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (
+      <header>
+        <h1>Rancid Tomatillos</h1>
+        <button>Login</button>
+      </header>
+    )
+  }
+}
+
+export default Header
 
