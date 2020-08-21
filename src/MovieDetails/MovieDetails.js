11f0efc112ba5104364f1d94aeeb1a12333fe5d8
Rachel Williams <rachelwilliams659@gmail.com>
2020-08-20 19:50:04 -0600

    Create movie details component

---
+++ src/MovieDetails/MovieDetails.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './MovieDetails.css'
+
+const MovieDetails = () => {
+  return (
+    <section className='MovieDetails'>
+      <section className='movie-poster-section'>
+        <img src='' alt=''/>
+      </section>
+      <section className='movie-info'>
+        <h2>Movie Title</h2>
+        <h3>Release date: release date</h3>
+        <h3>Average rating: average rating</h3>
+      </section>
+    </section>
+
+  )
+
+}
+
+export default MovieDetails 
