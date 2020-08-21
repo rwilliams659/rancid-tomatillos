6abc2c88127aedf3ce12aea33963c2d4bded908e
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-20 19:10:51 -0600

    Write first test for header component

---
+++ src/Header/Header.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import App from '../App/App.js'
+import Header from './Header.js';
+import { screen, fireEvent, render, getByLabelText }
+  from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('Header Component', () => {
+  it('should have the correct content when rendered', () => {
+    render(<Header
+      changeView={jest.fn()} 
+      loggedIn={false}
+      updateLoginStatus={jest.fn()} 
+      updateUserId={jest.fn()} 
+    />)
+    const button = screen.getByRole('button');
+    const heading = screen.getByRole('heading');
+    expect(button).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+  })
+
+  // it('should fire a function when the log in button is clicked', () => {
+
+  //   const mockChangeView = jest.fn()
+  //   const mockUpdateLoginStatus = jest.fn()
+  //   const mockUpdateUserId = jest.fn()
+  //   render(<Header
+  //     changeView={jest.fn()}
+  //     loggedIn= {true}
+  //     updateLoginStatus={jest.fn()}
+  //     updateUserId={jest.fn()}
+  //   />)
+  //   const button = screen.getByRole('button');
+  //   fireEvent.click(button); 
+
+  //   expect(mockChangeView).toBeCalledTimes(1);
+  //   expect(mockChangeView).toBeCalledWith('homepage');
+
+  //   expect(mockUpdateLoginStatus).toBeCalledTimes(1);
+  //   expect(mockUpdateLoginStatus).toBeCalledWith(false);
+
+  //   expect(mockUpdateUserId ).toBeCalledTimes(1);
+  //   expect(mockUpdateUserId ).toBeCalledWith(null);
+  // })
+
+})
