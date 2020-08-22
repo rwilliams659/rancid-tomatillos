5362f4dc5e2cf256f42f2efae4ba6358a6847c6f
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-22 09:57:47 -0600

    Add conditional rendering to display user rating or the rating form

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -5,7 +5,7 @@ class MovieDetails extends Component {
   constructor(props) {
     super(props) 
     this.state = {
-    ratedByUser: false
+    ratedByUser:false
     }
   }
 
@@ -15,34 +15,37 @@ class MovieDetails extends Component {
         <section className='movie-poster-section'>
           <img src={this.props.poster} alt={this.props.title} className='movie-details-img'/>
         </section>
+
         <section className='movie-info'>
           <h2>{this.props.title}</h2>
           <h3>Release date: {this.props.releaseDate}</h3>
           <h3>Average rating: {this.props.averageRating}</h3>
-          {/* Conditional rendering - is movie rated ?  */}
-          {/* TRUE: */}
-          <h3>Your rating: {this.props.averageRating}</h3>
-          {/* Delete rating functionality */}
-          {/* Conditional rendering - is movie rated ?  */}
-          {/* FALSE: */}
-          <form>
-            <select name='rateMovieDropdown'>
-              <option value=''>--Choose a rating--</option>
-              <option value='1'>1</option>
-              <option value='2'>2</option>
-              <option value='3'>3</option>
-              <option value='4'>4</option>
-              <option value='5'>5</option>
-              <option value='6'>6</option>
-              <option value='7'>7</option>
-              <option value='8'>8</option>
-              <option value='9'>9</option>
-              <option value='10'>10</option>
-            </select>
-            <input type='submit' value='Submit'/> 
-            {/* Add click event for submit button */}
-          </form>
-        </section>
+
+          {/* Conditional rendering - is movie rated ? TRUE/FALSE: */}
+          {this.state.ratedByUser ? (
+            <>
+              <h3>Your rating: {this.props.averageRating}</h3>
+              <button>Delete rating</button>
+           </>
+          ) : (
+            <form>
+              <select name='rateMovieDropdown'>
+                <option value=''>--Choose a rating--</option>
+                <option value='1'>1</option>
+                <option value='2'>2</option>
+                <option value='3'>3</option>
+                <option value='4'>4</option>
+                <option value='5'>5</option>
+                <option value='6'>6</option>
+                <option value='7'>7</option>
+                <option value='8'>8</option>
+                <option value='9'>9</option>
+                <option value='10'>10</option>
+              </select>
+              <input type='submit' value='Submit'/> 
+            </form>
+          )}
+          </section>
       </section>
     )
   }
