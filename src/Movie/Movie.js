41d323d1a5041a67dd80d1f66979920a6eebdbd4
Rachel Williams <rachelwilliams659@gmail.com>
2020-08-25 14:28:53 -0600

    Round average ratings on display

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -4,7 +4,7 @@ import './Movie.css'
 const Movie = ({id, title, averageRating, backdropPath, rating}) => {
   return (
     <section className='Movie' aria-label='movie' style={{ backgroundImage: `url(${backdropPath})`}} id={id}>
-      <p className="movie-rating">{averageRating} / 10</p>
+      <p className="movie-rating">{Math.round(averageRating * 10) / 10} / 10</p>
       <h3 className='movie-title'>{title}</h3>
       {rating &&
         <p className="user-rating">Your rating: {rating.rating} / 10</p>
