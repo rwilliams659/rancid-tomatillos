250b43ea0082c528e6256928a6b263c961503f07
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-28 18:29:09 -0600

    Create comment container component

---
+++ src/CommentContainer/CommentContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CommentContainer.css'
+
+
+const CommentContainer = (props) => {
+return (
+  <section className='CommentContainer'>
+     <p className='no-comments'>No comments to display</p>
+  </section>
+)}
+
+export default CommentContainer
