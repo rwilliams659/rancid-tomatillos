18e76bbc9be9202a34f79899ebe64dddd43cc159
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-29 09:16:03 -0600

    Add helper method for fetching comments onload and when new comment is added

--- src/CommentContainer/CommentContainer.js
+++ src/CommentContainer/CommentContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import './CommentContainer.css'
-// import CommentForm from '../CommentForm/CommentForm'
 import { postComment, getComments } from '../apiCalls'
 import Comments from '../Comments/Comments'
 import { Link } from 'react-router-dom'
@@ -8,7 +7,7 @@ import { Link } from 'react-router-dom'
 
 class CommentContainer extends Component {
   constructor({loggedIn, movieId}) {
-    super(loggedIn, movieId)
+    super()
     this.state = {
       author: '',
       comment: '',
@@ -17,19 +16,35 @@ class CommentContainer extends Component {
     }
   }
 
+  loadCommentsFromServer = (id) => {
+    getComments(id)
+   .then(comments => this.setState({ allComments: comments.comments }))
+    .catch(error => {
+      console.warn('Error loading comments');
+      this.setState({ error: 'Oops! Something went wrong!' })
+    })
+}
+
   componentDidMount() {
-    getComments(this.props.movieId)
-      .then(comments => this.setState({ allComments: comments.comments }))
-      .catch(error => {
-        console.warn('Error loading comments');
-        this.setState({ error: 'Oops! Something went wrong!' })
-      })
+    this.loadCommentsFromServer(this.props.movieId)
   }
 
-  addComment = () => {
+  resetForm() {
+    this.setState({
+      author: '',
+      comment: ''
+    })
+  } 
+
+  addComment = (event) => {
+    event.preventDefault()
+    console.log('add comment')
+    this.resetForm()
     postComment(this.props.movieId, this.state.author, this.state.comment)
       .then(response => {
         console.log(response)
+        this.loadCommentsFromServer(this.props.movieId)
+        console.log('updated comments:', this.state.comments)
       })
       .catch(error => {
         console.log(error);
@@ -47,15 +62,20 @@ class CommentContainer extends Component {
   render() {
     return (
       <section className='CommentContainer'>
-        {(this.state.allComments.length < 1) &&
-        <p className='no-comments'>No comments to display</p>
+        <h3> Comments </h3>
+        {!this.state.allComments.length &&
+          <div>
+            <p className='no-comments'>No comments to display</p>
+          </div>
         }
-        <Comments allComments={this.allComments} />
+        {(this.state.allComments.length > 0) &&
+        <Comments comments={this.state.allComments} />
+        }
         {this.props.loggedIn &&
         <form className='CommentForm' onChange={this.handleCommentSubmission}>
-          <h3> Add comment to movie: </h3>
+          <h3> Add comment: </h3>
           <input name='author' type='text' placeholder='Your name' />
-          <input name='comment' type='text' placeholder='Comments..' />
+          <input name='comment' type='text' placeholder='Write your comment here..' />
           <input type='submit' value='Post' onClick={this.addComment} />
         </form>
         }
