391ec55d4e3eaddb71de85ffd02147f75b465f43
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-28 19:41:02 -0600

    Add rendering for comment form and comments

--- src/CommentContainer/CommentContainer.js
+++ src/CommentContainer/CommentContainer.js
@@ -1,12 +1,32 @@
 import React from 'react'
 import './CommentContainer.css'
+import CommentForm from '../CommentForm/CommentForm'
+import Comments from '../Comments/Comments'
+import { Link } from 'react-router-dom'
 
 
-const CommentContainer = (props) => {
+const CommentContainer = ({loggedIn}) => {
+
+  // if there are no comments. Display message
+  // otherwise, map over comments and render comment boxes
+
+  // if logged in, show form for adding a new comment
+
 return (
   <section className='CommentContainer'>
-     <p className='no-comments'>No comments to display</p>
+   <p className='no-comments'>No comments to display</p>
+    
+      {loggedIn &&
+      <div>
+      <h3>Comments</h3>
+      <Comments />
+      <CommentForm />
+    </div>
+      }
+    
   </section>
 )}
+
+
 
 export default CommentContainer
