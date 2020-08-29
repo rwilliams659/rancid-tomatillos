8c7357e2cbfe3f9a2828c83c008b199a56819feb
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-28 21:09:49 -0600

    Create add comment method to handle post request

--- src/CommentForm/CommentForm.js
+++ src/CommentForm/CommentForm.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react'
+import { postComment } from '../apiCalls'
 
 
 class CommentForm extends Component {
-  constructor() {
-    super()
+  constructor({ movieId }) {
+    super(movieId)
     this.state = {
       author: '',
       comment: '',
+      error: ''
     }
   }
 
+  addComment = () => {
+    postComment(this.props.movieId, this.state.author, this.state.comment)
+    .then(response => {
+      console.log(response)
+    })
+    .catch(error => {
+      console.log(error);
+      this.setState({ error: 'Sorry, your comment could not be added.' })
+    })
+  }
+
+
   handleCommentSubmission = (event) => {
+    console.log('movieID:', this.props.movieId)
     const nameOfInput = event.target.name
     const valueOfInput = event.target.value
     this.setState({ [nameOfInput]: valueOfInput })
@@ -19,10 +34,10 @@ class CommentForm extends Component {
   render() {
     return (
       <form className='CommentForm' onChange={this.handleCommentSubmission}>
-        <h3> Add comment: </h3>
+        <h3> Add comment to movie: </h3>
         <input name='author' type='text' placeholder='Your name' />
         <input name='comment' type='text' placeholder='Comments..' />
-        <input type='submit' value='Post' />
+        <input type='submit' value='Post' onClick={this.addComment} />
       </form>
     );
   }
