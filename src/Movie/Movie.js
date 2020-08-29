c1241cab59b96b962d520a79dd5a78cb5f211ea5
Rachel Williams <rachelwilliams659@gmail.com>
2020-08-29 10:10:49 -0600

    Add favorite icons and style them

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './Movie.css'
+import heartFavoriteFalse from '../images/heart-outline.png'
+import heartFavoriteTrue from '../images/heart.png'
 
 const Movie = ({id, title, averageRating, backdropPath, rating}) => {
   return (
     <section className='Movie' aria-label='movie-overview' style={{ backgroundImage: `url(${backdropPath})`}} id={id}>
+      <img className = "heart" src={heartFavoriteFalse}></img>
       <p className="movie-rating">{Math.round(averageRating * 10) / 10} / 10</p>
       <h3 className='movie-title'>{title}</h3>
       {rating &&
