85b0bf277d35a1164440bd37010851155f472a68
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-29 17:39:14 -0600

    Refactor comments to a functional component

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -1,31 +1,25 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CommentBox from '../CommentBox/CommentBox'
 import PropTypes from 'prop-types'
 import './Comments.css'
 
-class Comments extends Component {
-  constructor({comments}) {
-    super()
-  }
-
-  render() {
-    let commentsList;
-    commentsList = this.props.comments.map(comment => {
-      return (
-        <CommentBox
-          comment={comment.comment}
-          author={comment.author}
-          key={comment.id}
-        />
-      )
-    })
+const Comments = ({ comments }) => {
+  let commentsList;
+  commentsList = comments.map(comment => {
+    return (
+      <CommentBox
+        comment={comment.comment}
+        author={comment.author}
+        key={comment.id}
+      />
+    )
+  })
     
     return (
       <section className='Comments'>
         {commentsList}
       </section>
     )
-  }
 }
 
 Comments.propTypes = {
