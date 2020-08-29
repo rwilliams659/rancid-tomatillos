433d1699b09c9ffd5e718aff0db9aff5c7025bb2
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-29 00:02:05 -0600

    Create comments component and styling

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
+import CommentBox from '../CommentBox/CommentBox'
+import './Comments.css'
 
-const Comments = () => {
-  return (
-    <h2>hi</h2>
-  )
+class Comments extends Component {
+  constructor({comments}) {
+    super()
+  }
+
+  render() {
+    let commentsList;
+    commentsList = this.props.comments.map(comment => {
+      return (
+        <CommentBox
+          comment={comment.comment}
+          author={comment.author}
+          key={comment.id}
+        />
+      )
+    })
+    
+    return (
+      <section className='Comments'>
+        {commentsList}
+      </section>
+    )
+  }
 }
 
 export default Comments
