34019683344aca6db25ad9c995a003c3dcd90109
Stephanie Norton <steph.jane.norton@gmail.com>
2020-08-28 21:47:45 -0600

    Combine comment form and comment container components

--- src/CommentContainer/CommentContainer.js
+++ src/CommentContainer/CommentContainer.js
@@ -1,32 +1,62 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './CommentContainer.css'
-import CommentForm from '../CommentForm/CommentForm'
+// import CommentForm from '../CommentForm/CommentForm'
+import { postComment, getComments } from '../apiCalls'
 import Comments from '../Comments/Comments'
 import { Link } from 'react-router-dom'
 
 
-const CommentContainer = ({loggedIn}) => {
+class CommentContainer extends Component {
+  constructor({loggedIn, movieId}) {
+    super(loggedIn, movieId)
+    this.state = {
+      author: '',
+      comment: '',
+      error: '',
+      allComments: []
+    }
+  }
 
-  // if there are no comments. Display message
-  // otherwise, map over comments and render comment boxes
+  componentDidMount() {
+    getComments(this.props.movieId)
+      .then(comments => this.setState({ allComments: comments.comments }))
+      .catch(error => {
+        console.warn('Error loading comments');
+        this.setState({ error: 'Oops! Something went wrong!' })
+      })
+  }
 
-  // if logged in, show form for adding a new comment
+  addComment = () => {
+    postComment(this.props.movieId, this.state.author, this.state.comment)
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({ error: 'Sorry, your comment could not be added.' })
+      })
+  }
 
-return (
-  <section className='CommentContainer'>
-   <p className='no-comments'>No comments to display</p>
-    
-      {loggedIn &&
-      <div>
-      <h3>Comments</h3>
-      <Comments />
-      <CommentForm />
-    </div>
-      }
-    
-  </section>
-)}
+  handleCommentSubmission = (event) => {
+    console.log('movieID:', this.props.movieId)
+    const nameOfInput = event.target.name
+    const valueOfInput = event.target.value
+    this.setState({ [nameOfInput]: valueOfInput })
+  }
 
-
+  render() {
+    return (
+      <section className='CommentContainer'>
+        <p className='no-comments'>No comments to display</p>
+        <form className='CommentForm' onChange={this.handleCommentSubmission}>
+          <h3> Add comment to movie: </h3>
+          <input name='author' type='text' placeholder='Your name' />
+          <input name='comment' type='text' placeholder='Comments..' />
+          <input type='submit' value='Post' onClick={this.addComment} />
+        </form>
+      </section >
+    );
+  }
+}
 
 export default CommentContainer
