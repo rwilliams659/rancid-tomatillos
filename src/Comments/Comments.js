04fd139037f6e4058167854f5ca7f1207ba8c507
Stephanie Norton <steph.jane.norton@gmail.com>
2020-09-01 16:36:55 -0600

    Refactor to clean up function

--- src/Comments/Comments.js
+++ src/Comments/Comments.js
@@ -5,15 +5,13 @@ import './Comments.css';
 
 const Comments = ({ comments }) => {
   let commentsList;
-  commentsList = comments.map(comment => {
-    return (
+  commentsList = comments.map(comment => 
       <CommentCard
         comment={comment.comment}
         author={comment.author}
         key={comment.id}
       />
-    )
-  })
+  )
     
   return (
     <section className='Comments'>
